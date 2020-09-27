9d0d20b1b6158d497485bf94957af11fdf0fbcd7
TdWa <59452572+TdWa@users.noreply.github.com>
2020-09-27 21:46:20 +0200

    upd

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,13 @@ b1.onclick = () => {
     b1.textContent = 'Light Theme'
     root.style.setProperty('--color1', 'rgb(61, 58, 58)');
     root.style.setProperty('--color2', 'rgb(61, 158, 58)');
-    root.style.setProperty('--color3', 'rgb(61, 58, 158)');
+    root.style.setProperty('--color3', 'rgb(248, 232, 6);');
     root.style.setProperty('--textColor', 'rgb(245, 245, 245)');    
   } else {
     b1.textContent = 'Dark Theme'
     root.style.setProperty('--color1', 'rgb(230, 230, 230)');
     root.style.setProperty('--color2', 'rgb(230, 223, 223)');
-    root.style.setProperty('--color3', 'rgb(109, 116, 180)');
+    root.style.setProperty('--color3', 'rgb(248, 232, 6);');
     root.style.setProperty('--textColor', 'rgb(10, 10, 10)');     
   }
 }
