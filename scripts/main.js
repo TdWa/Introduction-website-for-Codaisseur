56b42ff6e29da0f64e87ff2c673d4516deca4089
TdWa <59452572+TdWa@users.noreply.github.com>
2020-09-29 23:38:25 +0200

    upd

--- scripts/main.js
+++ scripts/main.js
@@ -6,15 +6,31 @@ const root = document.documentElement;
 b1.onclick = () => {
   if (b1.textContent === 'Dark Theme') {
     b1.textContent = 'Light Theme'
-    root.style.setProperty('--color1', 'rgb(61, 58, 58)');
-    root.style.setProperty('--color2', 'rgb(61, 158, 58)');
-    root.style.setProperty('--color3', 'rgb(248, 232, 6);');
-    root.style.setProperty('--textColor', 'rgb(245, 245, 245)');    
+    root.style.setProperty('--bgColor1', 'rgb(3, 2, 19)');
+    root.style.setProperty('--bgColor2', 'rgb(8, 23, 32)');
+    root.style.setProperty('--bgColor3', 'rgb(3, 2, 19)');
+    root.style.setProperty('--accentColor', 'rgb(167, 128, 0)');
+    root.style.setProperty('--textColor', 'rgb(255, 250, 238)');     
   } else {
     b1.textContent = 'Dark Theme'
-    root.style.setProperty('--color1', 'rgb(230, 230, 230)');
-    root.style.setProperty('--color2', 'rgb(230, 223, 223)');
-    root.style.setProperty('--color3', 'rgb(248, 232, 6);');
-    root.style.setProperty('--textColor', 'rgb(10, 10, 10)');     
+    root.style.setProperty('--bgColor1', 'rgb(255, 250, 238)');
+    root.style.setProperty('--bgColor2', 'rgb(212, 245, 212)');
+    root.style.setProperty('--bgColor3', 'rgb(219, 226, 224)');
+    root.style.setProperty('--accentColor', 'rgb(255, 238, 0)');
+    root.style.setProperty('--textColor', 'rgb(10, 6, 51)');    
   }
 }
+
+
+
+// --bgColor1: rgb(255, 250, 238);
+// --bgColor2: rgb(212, 245, 212);
+// --bgColor3: rgb(219, 226, 224);
+// --accentColor: rgb(255, 238, 0);
+// --textColor: rgb(10, 6, 51);
+
+// --bgColor1: rgb(3, 2, 19);
+// --bgColor2: rgb(8, 23, 32);
+// --bgColor3: rgb(3, 2, 19);
+// --accentColor: rgb(145, 107, 5);
+// --textColor: rgb(255, 250, 238);
